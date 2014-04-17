6e9099c409a583c26b7ef807fbd1f9c08e5fdbfc
bryan.absher <bryan.absher@lab49.com>
2014-04-16 22:13:53 -0400

    finished generator added simple sokoban

--- generateState.py
+++ generateState.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import sys
+import re
 
 filename = sys.argv[1]
 outFilename = sys.argv[2]
@@ -14,20 +15,26 @@ row = 0
 col = 0
 for line in file.readlines():
     col = 0
-    entries = line.split(' ')
+    entries = re.split('\s+', line)
     for entry in entries:
-        print '{0} at ({1},{2})'.format(entry, row, col)
-        entry = entry.upper()
-        if entry == '1' :
-            out.write('Obstacle({0}_{1})'.format(row, col))
-        elif entry == 'R1':
-            out.write('At(R1,{0}_{1})'.format(row, col))
-        elif entry == 'B':
-            out.write('BlockAt({0}_{1})')
-        else:
-            print entry
-            
-        if col != len(entries):
-            out.write(', ')
-        col += 1
+        if entry != '':
+            print '{0} at ({1},{2})'.format(entry, row, col)
+            entry = entry.upper()
+            if entry == '1' :
+                out.write('Obstacle({0}_{1})'.format(row, col))
+            elif entry == 'R1':
+                out.write('At(R1,{0}_{1})'.format(row, col))
+            elif entry == 'B':
+                out.write('BlockAt({0}_{1})'.format(row, col))
+            elif entry == 'O':
+                out.write('Open({0}_{1})'.format(row, col))
+            elif entry == 'E':
+                out.write('End({0}_{1}), '.format(row,col))
+                out.write('Open({0}_{1})'.format(row, col))
+            else:
+                print 'Unknown: {}'.format(entry)
+
+            if col != len(entries) and entry != '':
+                out.write(', ')
+            col += 1
     row += 1
